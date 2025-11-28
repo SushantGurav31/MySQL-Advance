51505a392d70bf384609a1492fb3258d5f192063
Sushant Gurav <sushantguravfsd@gmail.com>
2025-11-28 22:22:19 +0530

    Add files via upload

---
+++ world_bank.sql
@@ -0,0 +1,69 @@
+create database world_bank;
+
+use world_bank;
+
+
+-- Write a query to show all records from table.
+select * from world_bank_data_sql;   
+
+
+-- Display the list fo all countries in the dataset 
+select country_name from world_bank_data_sql;
+
+
+-- show the unique years available in the dataset 
+select distinct year from world_bank_data_sql;
+
+
+-- find the GDP value for a specific country (example: India)
+
+
+
+-- GDP and population for all countries in year 2020
+
+
+
+-- Records where population is greater than 10 million
+
+
+-- Countries where GDP per capita is NOT NULL
+
+
+-- Inflation (CPI %) for all countries in 2015
+
+
+
+-- Count how many records have NULL GDP
+
+
+
+
+-- Total number of records for each country
+
+
+
+
+-- Top 5 countries with the highest population
+
+
+
+
+-- Records where GDP growth (%) is negative
+
+
+
+
+-- Country, year, and GDP where GDP is above 1 trillion
+
+
+
+
+-- Countries where tax revenue (% of GDP) is less than 5%
+
+
+
+
+
+-- Records where inflation (CPI %) is equal to 0
+
+                       
